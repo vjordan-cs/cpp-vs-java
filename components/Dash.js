23a7deb6227ac1919a16c18daa29e063dcd3a674
Victoria Jordan <vjordan.cs@gmail.com>
2022-12-04 23:53:26 -0600

    insert all code

---
+++ components/Dash.js
@@ -0,0 +1,397 @@
+import { CodeBlock } from "@atlaskit/code";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const cppMain = `// free-floating function
+int main( int argc, char* argv[])
+{
+    printf( "Hello, world" );
+}`;
+const javaMain = `// every function must be part of a class; the main function for a particular
+// class file is invoked when java <class> is run (so you can have one
+// main function per class--useful for writing unit tests for a class)
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println( "Hello, World" );
+    }
+}`;
+const cppComp = `// compile as
+g++ foo.cc -o outfile
+// run with
+./outfile`;
+const javaComp = `// compile classes in foo.java to <classname>.class
+javac foo.java 
+
+// run by invoking static main method in <classname>
+java <classname>`;
+const cppClassDec = `class Bar {};`;
+const javaClassDec = `class Bar {}`;
+const staticMem = `class Foo 
+{
+    static private int x;
+    // static initialization block
+    { x = 5; }
+}`;
+
+const cppScope = `class MyClass
+{
+    public:
+    static doStuff();
+};
+
+// now it's used like this
+MyClass::doStuff();`;
+
+const javaScope = `class MyClass
+{
+    public static doStuff()
+    {
+        // do stuff
+    }
+}
+
+// now it's used like this
+MyClass.doStuff();`;
+const cppObj = `// on the stack
+myClass x;
+
+// or on the heap
+myClass *x = new myClass;`;
+const javaObj = `// always allocated on the heap (also, always need parens for constructor)
+myClass x = new myClass();`;
+
+const cppAccess = `// If you're using a stack-based object, you access its fields with a dot:
+myClass x;
+x.my_field; // ok
+
+// But you use the arrow operator (->) to access fields of a class when working with a pointer:
+myClass x = new MyClass;
+x->my_field; // ok`;
+const javaAccess = `myClass x = new MyClass();
+x.my_field; // ok`;
+const cppRef = `// references are immutable, use pointers for more flexibility
+int bar = 7, qux = 6;
+int& foo = bar;`;
+const javaRef = `// references are mutable and store addresses only to objects; there are
+// no raw pointers
+myClass x;
+x.foo(); // error, x is a null "pointer"
+
+// note that you always use . to access a field`;
+const cppInher = `class Foo : public Bar
+{ ... };`;
+const javaInher = `class Foo extends Bar
+{ ... }`;
+const cppProtect = `public:
+void foo();
+void bar();`;
+const javaProtect = `public void foo();
+public void bar();`;
+const cppVirtual = `virtual int foo(); // or, non-virtually as simply int foo();`;
+const javaVirtual = `// functions are virtual by default; use final to prevent overriding
+int foo(); // or, final int foo();`;
+const cppAbstract = `// just need to include a pure virtual function
+class Bar { public: virtual void foo() = 0; };`;
+const javaAbstract = `// syntax allows you to be explicit!
+abstract class Bar { public abstract void foo(); }
+
+// or you might even want to specify an interface
+interface Bar { public void foo(); }
+
+// and later, have a class implement the interface:
+class Chocolate implements Bar
+{
+    public void foo() { /* do something */ }
+}`;
+
+const cppNull = `// initialize pointer to NULL
+int *x = NULL;`;
+
+const javaNull = `// the compiler will catch the use of uninitialized references, but if you
+// need to initialize a reference so it's known to be invalid, assign null
+myClass x = null;`;
+
+const cppBool = `bool foo;`;
+
+const javaBool = `boolean foo;`;
+
+const cppConst = `const int x = 7;`;
+
+const javaConst = `final int x = 7;`;
+
+const cppThrow = `int foo() throw (IOException)`;
+
+const javaThrow = `int foo() throws IOException`;
+
+const cppArray = `int x[10];
+// or 
+int *x = new x[10];
+
+// use x, then reclaim memory
+delete[] x;`;
+
+const javaArray = `int[] x = new int[10];
+// use x, memory reclaimed by the garbage collector or returned to the
+// system at the end of the program's lifetime`;
+
+const cppCollec = `vector myVec;
+for ( vector<int>::iterator itr = myVec.begin();
+      itr != myVec.end();
+      ++itr )
+{
+    cout << *itr;
+}`;
+const javaCollec = `ArrayList myArrayList = new ArrayList();
+Iterator itr = myArrayList.iterator();
+while ( itr.hasNext() )
+{
+    System.out.println( itr.next() );
+}
+
+// or, in Java 5
+ArrayList myArrayList = new ArrayList();
+for( Object o : myArrayList ) {
+    System.out.println( o );
+}`;
+
+export default function Dash() {
+  return (
+    <div>
+      <div className="min-h-full w-full">
+        <div
+          as="nav"
+          className="bg-gray-800 text-white py-6 pl-36 text-xl font-bold"
+        >
+          C++ vs Java - Object Oriented Programming with Rick King
+        </div>
+
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl tracking-tight text-gray-900">Examples</h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className="px-4 py-6 sm:px-0">
+              {/* <List /> */}
+              <ul role="list" className="space-y-3">
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Main Function</div>
+                  <div className="">C++</div>
+                  <CodeBlock language="c++" text={cppMain} />
+                  <div className="">Java</div>
+                  <CodeBlock language="java" text={javaMain} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Compiling</div>
+                  C++
+                  <CodeBlock language="c++" text={cppComp} />
+                  Java
+                  <CodeBlock language="java" text={javaComp} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Comments</div>
+                  <div>{"Same in both languages (// and /* */ both work)"}</div>
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Class Declarations</div>
+                  <div>
+                    Almost the same, but Java does not require a semicolon
+                  </div>
+                  C++
+                  <CodeBlock language="c++" text={cppClassDec} />
+                  Java
+                  <CodeBlock language="java" text={javaClassDec} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Method Declarations</div>
+                  <div>
+                    Same, except that in Java, must always be part of a class,
+                    and may prefix with public/private/protected
+                  </div>
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    Constructors and destructors
+                  </div>
+                  <div>
+                    {
+                      "Constructor has same syntax in both (name of the class), Java has no exact equivalent of the destructor"
+                    }
+                  </div>
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    Static member functions and variables
+                  </div>
+                  <div>
+                    {
+                      "Same as method declarations, but Java provides static initialization blocks to initialize static variables (instead of putting a definition in a source code file):"
+                    }
+                  </div>
+                  <CodeBlock language="Java" text={staticMem} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    Scoping static methods and namespaces
+                  </div>
+                  C++
+                  <div>
+                    If you have a class and wish to refer to a static method,
+                    you use the form Class::method.
+                  </div>
+                  <CodeBlock language="c++" text={cppScope} />
+                  Java
+                  <div>
+                    All scoping in Java uses the . again, just like accessing
+                    fields of a class, so it's a bit more regular:
+                  </div>
+                  <CodeBlock language="java" text={javaScope} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Object declarations</div>
+                  C++
+                  <CodeBlock language="c++" text={cppObj} />
+                  Java
+                  <CodeBlock language="java" text={javaObj} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    Accessing fields of objects
+                  </div>
+                  C++
+                  <div>
+                    If you're using a stack-based object, you access its fields
+                    with a dot:
+                  </div>
+                  <CodeBlock language="c++" text={cppAccess} />
+                  Java
+                  <div>
+                    You always work with references (which are similar to
+                    pointers--see the next section), so you always use a dot:
+                  </div>
+                  <CodeBlock language="java" text={javaAccess} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    References vs. pointers
+                  </div>
+                  C++
+                  <CodeBlock language="c++" text={cppRef} />
+                  Java
+                  <CodeBlock language="java" text={javaRef} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Inheritance</div>
+                  C++
+                  <CodeBlock language="c++" text={cppInher} />
+                  Java
+                  <CodeBlock language="java" text={javaInher} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    {"Protection levels (abstraction barriers)"}
+                  </div>
+                  C++
+                  <CodeBlock language="c++" text={cppProtect} />
+                  Java
+                  <CodeBlock language="java" text={javaProtect} />
+                </li>
+
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Virtual functions</div>
+                  C++
+                  <CodeBlock language="c++" text={cppVirtual} />
+                  Java
+                  <CodeBlock language="java" text={javaProtect} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Abstract classes</div>
+                  C++
+                  <CodeBlock language="c++" text={cppVirtual} />
+                  Java
+                  <CodeBlock language="java" text={javaVirtual} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Memory Management</div>
+                  <div>
+                    Roughly the same--new allocates, but no delete in Java since
+                    it has garbage collection.
+                  </div>
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">NULL vs. null</div>
+                  C++
+                  <CodeBlock language="c++" text={cppNull} />
+                  Java
+                  <CodeBlock language="java" text={javaNull} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Booleans</div>
+                  <div>
+                    Java is a bit more verbose: you must write boolean instead
+                    of merely bool.
+                  </div>
+                  C++
+                  <CodeBlock language="c++" text={cppBool} />
+                  Java
+                  <CodeBlock language="java" text={javaBool} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Const-ness </div>
+                  C++
+                  <CodeBlock language="c++" text={cppConst} />
+                  Java
+                  <CodeBlock language="java" text={javaConst} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Throw Spec</div>
+                  <div>
+                    First, Java enforce throw specs at compile time--you must
+                    document if your method can throw an exception
+                  </div>
+                  C++
+                  <CodeBlock language="c++" text={cppThrow} />
+                  Java
+                  <CodeBlock language="java" text={javaThrow} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">Arrays</div>
+                  C++
+                  <CodeBlock language="c++" text={cppArray} />
+                  Java
+                  <CodeBlock language="java" text={javaArray} />
+                </li>
+                <li className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6">
+                  <div className="text-xl font-bold">
+                    Collections and Iteration
+                  </div>
+                  C++
+                  <div>
+                    {
+                      "Iterators are members of classes. The start of a range is <container>.begin(), and the end is <container>.end(). Advance using ++ operator, and access using *."
+                    }
+                  </div>
+                  <CodeBlock language="c++" text={cppCollec} />
+                  Java
+                  <div>
+                    {
+                      "Iterator is just an interface. The start of the range is <collection>.iterator, and you check to see if you're at the end with itr.hasNext(). You get the next element using itr.next() (a combination of using ++ and * in C++)."
+                    }
+                  </div>
+                  <CodeBlock language="java" text={javaCollec} />
+                </li>
+              </ul>
+            </div>
+            {/* /End replace */}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ components/Footer.js
@@ -0,0 +1,73 @@
+const navigation = [
+  {
+    name: "GitHub",
+    href: "https://github.com/vjordan-cs/cpp-vs-java",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/victoria-jordan01/",
+    icon: (props) => (
+      <svg x="0px" y="0px" width="26" height="26" viewBox="0 0 192 192">
+        {
+          <g
+            fill="none"
+            fill-rule="nonzero"
+            stroke="none"
+            stroke-width="1"
+            stroke-linecap="butt"
+            stroke-linejoin="miter"
+            stroke-miterlimit="10"
+            stroke-dasharray=""
+            stroke-dashoffset="0"
+            font-family="none"
+            font-weight="none"
+            font-size="none"
+            text-anchor="none"
+          >
+            <path d="M0,192v-192h192v192z" fill="none"></path>
+            <g fill="#9ba3af">
+              <g id="surface1">
+                <path d="M156,0h-120c-19.875,0 -36,16.125 -36,36v120c0,19.875 16.125,36 36,36h120c19.875,0 36,-16.125 36,-36v-120c0,-19.875 -16.125,-36 -36,-36zM59.36539,162.98077h-29.82693l-0.17307,-89.30769h29.82692zM43.70192,61.99038h-0.17308c-9.75,0 -16.03846,-6.72115 -16.03846,-15.08653c0,-8.56731 6.49039,-15.0577 16.41347,-15.0577c9.92308,0 16.00961,6.49038 16.21153,15.0577c0,8.36538 -6.31731,15.08653 -16.41346,15.08653zM162.77885,162.98077h-30.08654v-48.51923c0,-11.74039 -3.11538,-19.73077 -13.61538,-19.73077c-8.01923,0 -12.34615,5.39423 -14.42308,10.61538c-0.77885,1.875 -0.98077,4.44231 -0.98077,7.06731v50.56731h-30.23077l-0.17308,-89.30769h30.23077l0.17308,12.60577c3.86538,-5.97116 10.29808,-14.42308 25.70192,-14.42308c19.09616,0 33.37501,12.46154 33.37501,39.25961v51.86539z"></path>
+              </g>
+            </g>
+          </g>
+        }
+      </svg>
+    ),
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className="bg-white">
+      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-base text-gray-400">
+            &copy; 2022 Victoria Jordan. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
